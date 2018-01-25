ef81d3b57a22453583b623c3110ae647c080deaa
Jakub Juřena <445319@mail.muni.cz>
2018-01-25 23:18:17 +0100

    redux and login with google or fb added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+import * as actions from "../actions";
 
 /**
  *  MOCKUP COMPONENTS
  */
-const Navbar = () => <h1>Navbar component</h1>;
-const Home = () => <h2>Home component</h2>;
-const Login = () => <h2>Login component</h2>;
-const Profile = () => <h2>Profile component</h2>;
-const MovieLists = () => <h2>MovieLists component</h2>;
-const MovieListDetail = () => <h2>MovieListDetail component</h2>;
-const MovieDetail = () => <h2>MovieDetail component</h2>;
-const Friends = () => <h2>Friends component</h2>;
+import Navbar from "./NavBar";
+import LoginPage from "./pages/LoginPage";
+const HomePage = () => <h2>HomePage component</h2>;
 
+const DashboarPage = () => <h2>DashboarPage component</h2>;
+const ProfilePage = () => <h2>ProfilePage component</h2>;
+const MovieListsPage = () => <h2>MovieListsPage component</h2>;
+const MovieListDetailPage = () => <h2>MovieListDetailPage component</h2>;
+const MovieDetailPage = () => <h2>MovieDetailPage component</h2>;
+const FriendsPage = () => <h2>FriendsPage component</h2>;
+
+/**
+ *  APP CLASS
+ */
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  checkAuth(nextState, replaceState) {
+    const user = this.props.auth;
+
+    if (user) {
+      replaceState(null, nextState.location.pathname);
+    } else {
+      replaceState(null, "/");
+    }
+  }
+
   render() {
     return (
       <div>
@@ -21,13 +43,23 @@ class App extends Component {
           <div>
             <Navbar />
             <div className="container">
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profile" component={Profile} />
-              <Route exact path="/movie/lists" component={MovieLists} />
-              <Route exact path="/movie/list" component={MovieListDetail} />
-              <Route exact path="/movie" component={MovieDetail} />
-              <Route exact path="/friends" component={Friends} />
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/login" component={LoginPage} />
+              {/*TODO fix checkAuth ---> it doesn't work*/}
+              <Route onEnter={this.checkAuth}>
+                <div>
+                  <Route exact path="/dashboard" component={DashboarPage} />
+                  <Route exact path="/profile" component={ProfilePage} />
+                  <Route exact path="/movie/lists" component={MovieListsPage} />
+                  <Route
+                    exact
+                    path="/movie/list"
+                    component={MovieListDetailPage}
+                  />
+                  <Route exact path="/movie" component={MovieDetailPage} />
+                  <Route exact path="/friends" component={FriendsPage} />
+                </div>
+              </Route>
             </div>
           </div>
         </BrowserRouter>
@@ -36,4 +68,10 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,24 @@
+// temporary css library
+import "materialize-css/dist/css/materialize.min.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+//axios for api tests from browser
+import axios from "axios";
+window.axios = axios;
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);

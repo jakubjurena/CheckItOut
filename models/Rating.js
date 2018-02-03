3cbde36c5e6ca1056f7ba16aaf6ea1488b85e4fc
Jakub Juřena <445319@mail.muni.cz>
2018-02-03 21:08:58 +0100

    models added to index.js, collection names fixed

--- models/Rating.js
+++ models/Rating.js
@@ -3,10 +3,12 @@ const { Schema } = mongoose;
 
 const { visibilityEnum } = require("../config/enums");
 
-const filmRatingSchema = new Schema({
+const movieRatingSchema = new Schema({
   _film: { type: Schema.Types.ObjectId, ref: "Film" },
   _publisher: { type: Schema.Types.ObjectId, ref: "User" },
   comment: String,
   rating: Number,
   visibility: { type: String, enum: visibilityEnum, default: "friends"}
 });
+
+mongoose.model("Rating", movieRatingSchema);
